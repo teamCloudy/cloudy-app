2898c697338ba3c29c196ed0ffde9603024fbe39
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-11-17 17:38:25 -0800

    added more tests for weather and cities

--- app/javascript/components/pages/ProtectedCityIndex.test.js
+++ app/javascript/components/pages/ProtectedCityIndex.test.js
@@ -1,12 +1,28 @@
 import React from "react";
 import { render } from "@testing-library/react"
 import ProtectedCityIndex from "./ProtectedCityIndex"
-import cities from "../mockUserCities"
-import weathers from "../mockWeathers"
+import mockUserCities from "../mockUserCities"
+import mockWeathers from "../mockWeathers"
+import '@testing-library/jest-dom'
 
 describe("<ProtectedCityIndex />", () => {
-    it ("renders without crashing", () => {
+
+    it("renders without crashing", () => {
         const div = document.createElement("div")
-        render(<ProtectedCityIndex cities={cities} weathers={weathers}  />, div)
+        render(<ProtectedCityIndex />, div)
     })
+
+
+    it("renders without crashing with mockUserCities", () => {
+        const div = document.createElement("div");
+        render(<ProtectedCityIndex mockUserCities={mockUserCities} />, div);
+    });
+
+    it("renders without crashing with mockWeathers", () => {
+        const div = document.createElement("div");
+        render(<ProtectedCityIndex mockWeathers={mockWeathers} />, div);
+    });
+
+
 })
+
