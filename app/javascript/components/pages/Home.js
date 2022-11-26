97afae11ccbb5ef6adf2f4509bed5f395388db67
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-11-25 16:54:46 -0800

    forgot password form styled

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { ButtonToolbar } from "reactstrap";
 
-const Home =  ({new_user_route, sign_in_route, logged_in}) => {
+const Home =  ({new_user_route, sign_in_route, logged_in, current_user }) => {
     const navigate = useNavigate()
     const handleSubmit = (id) => {
         if (id === "index") {
@@ -12,16 +12,17 @@ const Home =  ({new_user_route, sign_in_route, logged_in}) => {
         }
     }
 
-
+    
 
     return (
         <>
+        
         <h3> Cloudy Weather App</h3>
 
         {/* Show when LOGGED IN */}
         {logged_in && (
             <div>
-                <h4> WELCOME USER!!</h4>
+                <h4> WELCOME {current_user.username}!!</h4>
                 <p>This is our app called CLOUDY. We are your personal weather app, to help you keep tabs of your favorite locations in the world. Whether you have friends and family all over the world, or you travel often, our app will help you stay informed 24/7. So what makes us different from our competitors? You can enjoy our app, ad free, we won't sell your personal data to advertisers. All of your locations will be private to your account. Your personal data is secured on our website.</p>
                 <button onClick={() => handleSubmit("user-index")} >My cities</button>
                 <button onClick={() => handleSubmit("CityIndex")}>All Cities</button>
