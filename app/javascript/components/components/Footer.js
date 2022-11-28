72fa5ad3dfdc5bb90360c5bb920d711d8c198712
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-11-27 21:54:58 -0800

    show pages styled notes styled bg images changed

--- app/javascript/components/components/Footer.js
+++ app/javascript/components/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 const Footer = () => {
     return (
 
-                <footer style={{ width: "100%", paddingBottom: "20px", textAlign: "center", color: "black", paddingTop: "20px", fontFamily: "Arial", fontSize: "1em",  background: "#D6DBDF", height: "50px", margin: "auto"}}>
+                <footer style={{ width: "100%", paddingBottom: "20px", textAlign: "center", color: "white", paddingTop: "20px", fontFamily: "Arial", fontSize: "1em",   background: "#000000", height: "50px", margin: "auto"}}>
                     Copyright &copy; 2022 | CLOUDY<br>
                     </br>
                 </footer>
--- app/javascript/components/pages/CityShow.js
+++ app/javascript/components/pages/CityShow.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams, NavLink, useNavigate } from "react-router-dom";
-import { Card, CardBody, CardTitle, CardSubtitle, Button } from "reactstrap";
+import { Card, CardBody, CardTitle, CardSubtitle, Button, Row, Col, CardImg, CardImgOverlay } from "reactstrap";
 
 const CityShow = ({ cities, weathers }) => {
   const { id } = useParams();
@@ -11,50 +11,68 @@ const CityShow = ({ cities, weathers }) => {
   );
   return (
     <>
-      <div className="cityShowPage">
-        <h3 className="centerTitle"> More Details</h3>
-        {showCity && (
-          <div className="centerCard">
-            <Card style={{ width: "250px", height: "auto" }}>
-              <CardBody>
-                <CardTitle>
-                  <h4>{showCity.city_name}</h4>
-                </CardTitle>
-                <CardSubtitle>
-                  <h6>{showCity.country_name}</h6>
-                  <br />
-                  <h3>{weather[0].temperature} &#176;</h3>
-                  <br />
-                  <strong>Last Updated: </strong>
-                  <br />
-                  <p>{weather[0].date}</p>
-                  <h5>{weathers[0].time}</h5>
-                  <br />
-                  <strong>Weather: </strong>
-                  {weather[0].current_weather}
-                  <img src={weather[0].icon} />
-                  <br />
-                  <strong>UV Index: </strong>
-                  {weather[0].uv_index}
-                  <br />
-                  <strong>Wind Speed: </strong>
-                  {weather[0].wind_speed}
-                  <br />
-                  <strong>Humidity: </strong>
-                  {weather[0].humidity}%
-                  <br />
-                  <strong>Description: </strong>
-                  <br />
-                  {weather[0].description}
-                  <br />
-                </CardSubtitle>
-                <NavLink to="/cityindex" className="nav-link">
-                  <Button>Back</Button>
-                </NavLink>
-              </CardBody>
-            </Card>
-          </div>
-        )}
+    <div class="show-main">
+      <div class="bg-image show-city-bg ">
+        <div class="mask-popular-cities">
+
+          <h1 className="popular-cities-title">MORE DETAILS</h1>
+          <main className="show-city-cards">
+
+            {showCity && (
+              <Row>
+                <Col sm="4">
+
+                  <Card className="card-show card-show-bg-image">
+                    <div class="mask-card-show-city">
+
+                      <CardBody>
+                        <CardTitle>
+                          <h4>{showCity.city_name}</h4>
+                        </CardTitle>
+                        <CardSubtitle>
+                          {showCity.country_name}
+                          <br />
+                          <img src={weather[0]?.icon} />
+                          <h3>{weather[0].temperature} &#176;</h3>
+                          <br />
+                          <strong>Last Updated: </strong>
+                          <br />
+                          <p>{weather[0].date}</p>
+                          <h5>{weathers[0].time}</h5>
+
+                          <hr className="my-4 separate-show" />
+                          <strong>Weather: </strong>
+                          {weather[0].current_weather}
+
+                          <br />
+                          <strong className="my-4">UV Index: </strong>
+                          {weather[0].uv_index}
+                          <br />
+                          <strong>Wind Speed: </strong>
+                          {weather[0].wind_speed}
+                          <br />
+                          <strong>Humidity: </strong>
+                          {weather[0].humidity}%
+                          <br />
+                          <strong>Description: </strong>
+                          <br />
+                          {weather[0].description}
+                          <br />
+                        </CardSubtitle>
+                        <NavLink to="/cityindex" className="nav-link">
+                          <Button className="back-btn-show">Back</Button>
+                        </NavLink>
+                      </CardBody>
+                    </div>
+                  </Card>
+                </Col>
+              </Row>
+
+            )}
+
+          </main>
+        </div>
+      </div>
       </div>
     </>
   );
